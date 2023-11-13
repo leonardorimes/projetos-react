f07b69423880a30f3d3dd267da4d5690954fb539
Leonardo Rimes <rimesleo@gmail.com>
2023-11-13 09:10:54 -0300

    estruturando o teclado, aplicando estilos

--- 1_calculadora/calculadora/src/components/Calculator.jsx
+++ 1_calculadora/calculadora/src/components/Calculator.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import "./Calculator.css";
 
 const Calculator = () => {
+  const keypadNumbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const operations = ["+", "-", "*", "/"];
+
   return (
     <div className="calculator">
-      <div className="complete-operation"> 3 + 3 = 6</div>
-      <div className="display">000</div>
-      <div className="buttons"></div>
-      <button> =</button>
+      <div className="complete-operation"> </div>
+      <div className="display"></div>
+      <div className="buttons">
+        <button> AC </button>
+        {keypadNumbers.map((num) => (
+          <button key={num}>{num}</button>
+        ))}
+        {operations.map((operation) => (
+          <button key={operation}>{operation}</button>
+        ))}
+        <button> =</button>
+      </div>
     </div>
   );
 };
