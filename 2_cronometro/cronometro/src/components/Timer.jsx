b4d421ba6af0dc50cd808ee3266f767a3a01aa30
Leonardo Rimes <rimesleo@gmail.com>
2023-11-18 17:05:23 -0300

    concluindo lógicas nos botoes

--- 2_cronometro/cronometro/src/components/Timer.jsx
+++ 2_cronometro/cronometro/src/components/Timer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import TimerDisplay from "./TimerDisplay";
 import TimerControls from "./TimerControls";
 import LapList from "./LapList";
@@ -6,10 +6,63 @@ import LapList from "./LapList";
 import "./Timer.css";
 
 const Timer = () => {
+  const [milliseconds, setMilliseconds] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+  const [laps, setLaps] = useState([]);
+
+  const formatTime = () => {
+    const minutes = ("0" + Math.floor((milliseconds / 60000) % 60)).slice(-2);
+    const seconds = ("0" + (Math.floor(milliseconds / 1000) % 60)).slice(-2);
+    const centiseconds = ("0" + (Math.floor(milliseconds / 10) % 100)).slice(
+      -2
+    );
+
+    return `${minutes}: ${seconds}: ${centiseconds}`;
+  };
+
+  const startTimer = (interval) => {
+    return setInterval(() => {
+      setMilliseconds((prevMilliseconds) => prevMilliseconds + 10);
+    }, 10);
+  };
+
+  const stopTimer = (interval) => {
+    clearInterval(interval);
+    return interval;
+  };
+
+  const resetTimer = () => {
+    setMilliseconds(0);
+    setTimerOn(false);
+    setLaps([]);
+  };
+
+  const addLap = () => {
+    setLaps([...laps, formatTime()]);
+  };
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = startTimer(interval);
+    }
+
+    return () => stopTimer(interval);
+  }, [timerOn]);
+
+  console.log(laps);
+
   return (
     <div className="timer-container">
-      <TimerDisplay />
-      <TimerControls />
+      <TimerDisplay time={formatTime()} />
+      <TimerControls
+        timerOn={timerOn}
+        onStart={() => setTimerOn(true)}
+        onStop={() => setTimerOn(false)}
+        onReset={resetTimer}
+        onLap={addLap}
+      />
       <LapList />
     </div>
   );
