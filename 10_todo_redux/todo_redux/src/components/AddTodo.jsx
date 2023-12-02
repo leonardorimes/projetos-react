3775a9fc7eb56b76d861df647d5ca46f4e4c7002
Leonardo Rimes <rimesleo@gmail.com>
2023-12-02 18:25:04 -0300

    adicionando tarefas

--- 10_todo_redux/todo_redux/src/components/AddTodo.jsx
+++ 10_todo_redux/todo_redux/src/components/AddTodo.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../slices/todoSlice";
+import { useState } from "react";
 
 const AddTodo = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim() === "") return;
+    dispatch(addTodo(input));
+    setInput("");
+  };
   return (
-    <form action="">
-      <input type="text" placeholder="Adicione uma tarefa" />
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Adicione uma tarefa"
+      />
       <button type="submit">Enviar</button>
     </form>
   );
