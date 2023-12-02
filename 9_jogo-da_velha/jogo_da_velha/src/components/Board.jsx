25e8cc43cc6ab21765abe2a1b61e1c1668215d9e
Leonardo Rimes <rimesleo@gmail.com>
2023-12-02 18:16:54 -0300

    implementando redux

--- 9_jogo-da_velha/jogo_da_velha/src/components/Board.jsx
+++ 9_jogo-da_velha/jogo_da_velha/src/components/Board.jsx
@@ -2,29 +2,39 @@ import { useEffect, useState } from "react";
 import Square from "./Square";
 
 const Board = () => {
-  const [squares, setSquares] = useState(Array(9).fill(null))
+  const [squares, setSquares] = useState(Array(9).fill(null));
   const [isNext, setIsNext] = useState(true);
   const winner = calculateWinner(squares);
 
-  const [aiIsThinking, setaiIsThinking] = useState(false)
+  const [aiIsThinking, setaiIsThinking] = useState(false);
 
   const handleClick = (i) => {
-    if(squares[i] || winner || aiIsThinking) return;
+    if (squares[i] || winner || aiIsThinking) return;
 
-    const newSquares = squares.slice()
+    const newSquares = squares.slice();
 
+    newSquares[i] = isNext ? "X" : "O";
 
-    newSquares[i] = isNext ? "X" : "O"
+    setSquares(newSquares);
 
-    setSquares(newSquares)
-
-    setIsNext(!isNext)
-  }
+    setIsNext(!isNext);
+  };
 
   const resetGame = () => {
-    setSquares(Array(9).fill(null))
-    setIsNext(true)
-  }
+    setSquares(Array(9).fill(null));
+    setIsNext(true);
+  };
+
+  useEffect(() => {
+    if (!isNext && !winner) {
+      setaiIsThinking(true);
+      setTimeout(() => {
+        aiMove(squares, setSquares, setIsNext);
+
+        setaiIsThinking(false);
+      }, 1000);
+    }
+  }, [isNext, squares, winner]);
 
   return (
     <div>
@@ -37,25 +47,25 @@ const Board = () => {
         )}
       </div>
       <div className="board-row">
-        <Square value={squares[0]} onClick={() => handleClick(0)}/>
-        <Square value={squares[1]} onClick={() => handleClick(1)}/>
-        <Square value={squares[2]} onClick={() => handleClick(2)}/>
+        <Square value={squares[0]} onClick={() => handleClick(0)} />
+        <Square value={squares[1]} onClick={() => handleClick(1)} />
+        <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
       <div className="board-row">
-        <Square value={squares[3]} onClick={() => handleClick(3)}/>
-        <Square value={squares[4]} onClick={() => handleClick(4)}/>
-        <Square value={squares[5]} onClick={() => handleClick(5)}/>
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
       </div>
       <div className="board-row">
-        <Square value={squares[6]} onClick={() => handleClick(6)}/>
-        <Square value={squares[7]} onClick={() => handleClick(7)}/>
-        <Square value={squares[8]} onClick={() => handleClick(8)}/>
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
       </div>
 
-      <button className="reset-button" onClick={resetGame}>Reiniciar Jogo</button>
+      <button className="reset-button" onClick={resetGame}>
+        Reiniciar Jogo
+      </button>
     </div>
-
-
   );
 };
 
@@ -71,18 +81,36 @@ const calculateWinner = (squares) => {
     [2, 4, 6],
   ];
 
-  for(let i =0; i < lines.length; i++){
-    const [a,b,c] = lines[i]
-    
-   
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a]
-    }
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
 
-   
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
   }
 
-  return null
-}
+  return null;
+};
+
+const aiMove = (squares, setSquares, setIsNext) => {
+  let move = null;
+
+  for (let i = 0; i < squares.length; i++) {
+    if (!squares[i]) {
+      move = i;
+      break;
+    }
+  }
+
+  if (move !== null) {
+    const newSquares = squares.slice();
+
+    newSquares[move] = "O";
+
+    setSquares(newSquares);
+
+    setIsNext(true);
+  }
+};
 
 export default Board;
