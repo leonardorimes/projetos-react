9605cdeb4679d9f05560d3fd2e37b6ed5025f9cd
Leonardo Rimes <rimesleo@gmail.com>
2023-12-05 07:38:50 -0300

    estilando com chacra

---
+++ 13_pomodoro/pomodoro/src/App.jsx
@@ -0,0 +1,17 @@
+import PomodoroTimer from "./components/PomodoroTimer";
+
+import { ChakraProvider, CSSReset, Box, Text } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <ChakraProvider>
+      <CSSReset />
+      <Box textAlign="center" fontSize="xl" mt="4">
+        <Text> Pomodoro</Text>
+        <PomodoroTimer />
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
---
+++ 13_pomodoro/pomodoro/src/components/PomodoroTimer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PomodoroTimer = () => {
+  return <div>PomodoroTimer</div>;
+};
+
+export default PomodoroTimer;
