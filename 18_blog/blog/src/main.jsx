a705d76ca3384a69ecda479d7d9aa072140a0331
Leonardo Rimes <rimesleo@gmail.com>
2023-12-12 07:16:37 -0300

    configurando o React Router

--- 18_blog/blog/src/main.jsx
+++ 18_blog/blog/src/main.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+
+// páginas
+import Home from "./routes/Home";
+import NewPost from "./routes/NewPost";
+
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/new",
+        element: <NewPost />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
