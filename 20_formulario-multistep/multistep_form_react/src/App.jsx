80454cb044c3a08633511cca3da46dcd0c6b4c39
Leonardo Rimes <rimesleo@gmail.com>
2023-12-15 19:11:38 -0300

    funcionalidade de mudar de etapas

--- 20_formulario-multistep/multistep_form_react/src/App.jsx
+++ 20_formulario-multistep/multistep_form_react/src/App.jsx
@@ -1,5 +1,6 @@
 // Components
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+import { FiSend } from "react-icons/fi";
 import UserForm from "./components/UserForm";
 import ReviewForm from "./components/ReviewForm";
 import Thanks from "./components/Thanks";
@@ -11,7 +12,8 @@ import "./App.css";
 
 function App() {
   const FormComponents = [<UserForm />, <ReviewForm />, <Thanks />];
-  const { currentStep, currentComponent } = useForm(FormComponents);
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } =
+    useForm(FormComponents);
   return (
     <div className="app">
       <div className="header">
@@ -23,17 +25,26 @@ function App() {
       </div>
       <div className="form-container">
         <p>etapas</p>
-        <form>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
           <div className="inputs-container"> {currentComponent} </div>
           <div className="actions">
-            <button type="button">
-              <GrFormPrevious />
-              <span>Voltar</span>
-            </button>
-            <button type="submit">
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isFirstStep && (
+              <button type="button" onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type="submit">
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type="button">
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
