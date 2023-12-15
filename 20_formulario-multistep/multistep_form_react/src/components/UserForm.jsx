d59e64bb382945da595fe9b6d74673523b54514c
Leonardo Rimes <rimesleo@gmail.com>
2023-12-15 19:33:41 -0300

    estilos iniciais

--- 20_formulario-multistep/multistep_form_react/src/components/UserForm.jsx
+++ 20_formulario-multistep/multistep_form_react/src/components/UserForm.jsx
@@ -1,7 +1,26 @@
 const UserForm = () => {
   return (
     <div>
-      <h2>userForm</h2>
+      <div className="form-control">
+        <label htmlFor="name">Nome:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Digite o seu nome"
+          required
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="email">E-mail:</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Digite o seu email"
+          required
+        />
+      </div>
     </div>
   );
 };
