07bbef41b7549bc5fbb2ee0a043d55be0196d801
Leonardo Rimes <rimesleo@gmail.com>
2023-12-20 18:57:11 -0300

    busca atraves da tecla enter

--- 22_githubFinder/github_finder/src/components/Search.tsx
+++ 22_githubFinder/github_finder/src/components/Search.tsx
@@ -2,7 +2,7 @@ type SearchProps = {
   loadUser: (userName: string) => Promise<void>;
 };
 
-import { useState } from "react";
+import { useState, KeyboardEvent } from "react";
 
 import { BsSearch } from "react-icons/bs";
 
@@ -10,6 +10,13 @@ import classes from "./Search.module.css";
 
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === "Enter") {
+      loadUser(userName);
+    }
+  };
+
   return (
     <div className={classes.search}>
       <h2>Busque por Usuário</h2>
@@ -19,6 +26,7 @@ const Search = ({ loadUser }: SearchProps) => {
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}
+          onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           <BsSearch />
