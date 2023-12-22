4f1936d6f7236af126913d71df94b991e57fa5dc
Leonardo Rimes <rimesleo@gmail.com>
2023-12-22 14:39:36 -0300

    css da pagina de repositorios

--- 22_githubFinder/github_finder/src/routes/Repos.tsx
+++ 22_githubFinder/github_finder/src/routes/Repos.tsx
@@ -5,6 +5,7 @@ import { useParams } from "react-router-dom";
 
 import BackBtn from "../components/BackBtn";
 import Loader from "../components/Loader";
+import Repo from "../components/Repo";
 
 import classes from "./Repos.module.css";
 
@@ -35,14 +36,14 @@ const Repos = () => {
   if (!repos && isLoading) return <Loader />;
 
   return (
-    <div>
+    <div className={classes.repos}>
       <BackBtn />
       <h2>Explore os repositórios do usuário: {username}</h2>
       {repos && repos.length === 0 && <p>Não há repositórios</p>}
       {repos && repos.length > 0 && (
-        <div>
+        <div className={classes.repos_container}>
           {repos.map((repo: RepoProps) => (
-            <p>{repo.name}</p>
+            <Repo key={repo.name} {...repo} />
           ))}
         </div>
       )}
