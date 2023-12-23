2f6750f732083389824ff2edcc4c7d70e211b810
Leonardo Rimes <rimesleo@gmail.com>
2023-12-23 14:41:50 -0300

    criando hook do formulario

---
+++ 23_react_hook_form/react_hook_form/src/schemas/FormSchema.ts
@@ -0,0 +1,21 @@
+import {z} from 'zod';
+
+export const formSchema = z.object({
+    name: z.string().min(3, 'O número minimo de caractere é 3'),
+    lastname: z.string().min(3, 'O número minimo de caractere é 3'),
+    gender: z.string().refine((field) => field !== "select", {
+        message: "Você precisa escolher um gênero"
+    }),
+    email: z.string().min(1, "O campo é obrigatório").email("Utilize um email válido"),
+    password: z.string().min(6, "A senha precisa ter no minimo 6 caracteres"),
+    confirmpassword: z.string().min(6, "A confirmação precisa ter no minimo 6 caracteres"),
+    agree: z.boolean().refine((field) => field === true, {
+        message: "Você precisa concordar com os termos"
+    })
+
+}).refine((field) => field.password === field.confirmpassword, {
+    message: "As senhas precisam ser iguais.",
+    path: ["confirmpassword"]
+})
+
+export type FormSchema = z.infer<typeof formSchema>;
