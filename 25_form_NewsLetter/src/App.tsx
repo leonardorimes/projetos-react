c6bbb4a1bab9611a345010e13ccc6780d29c63cc
Leonardo Rimes <rimesleo@gmail.com>
2023-12-27 14:34:07 -0300

    criando elementos

--- 25_form_NewsLetter/src/App.tsx
+++ 25_form_NewsLetter/src/App.tsx
@@ -2,10 +2,17 @@ import Form from "./components/Form";
 
 function App() {
   return (
-    <>
+    <div>
       <h1 className="text-red-600">Inscreva-se</h1>
-      <Form />
-    </>
+      <p>Assine nossa Newsletter e mantenha-se informado</p>
+      <div>
+        <Form />
+      </div>
+      <p>
+        Ao se inscrever, você passará a recever os nossos e-mails com as
+        melhores dicas, novidades e ofertas
+      </p>
+    </div>
   );
 }
 
--- 25_form_NewsLetter/src/components/Form.tsx
+++ 25_form_NewsLetter/src/components/Form.tsx
@@ -1,5 +1,24 @@
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <form>
+      <div>
+        <label> Nome: </label>
+        <input type="text" placeholder="Digite o seu nome: " />
+      </div>
+      <div>
+        <label> E-mail: </label>
+        <input type="email" placeholder="Insira o seu melhor e-mail" />
+      </div>
+      <div>
+        <a href="#">Leia os termos</a>
+        <div>
+          <input type="checkbox" />
+          <label>Concordo com os termos </label>
+        </div>
+      </div>
+      <button>Cadastrar</button>
+    </form>
+  );
 };
 
 export default Form;
